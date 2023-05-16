23e902ce535db970680d7baebe631b5c1576cdde
Aiman Sdia <102213880+AimanSadia@users.noreply.github.com>
2023-05-16 08:46:08 +0500

    Add files via upload

---
+++ ass3.js
@@ -0,0 +1,34 @@
+
+/* -----------------------ASSIGNMENT 03 ------------------------------ */
+
+/* ===========QUESTION NO 01 =============*/
+
+/* var age = 24;
+alert(`I am ${age} years old`); */
+
+
+/* ===========QUESTION NO 02 =============*/
+
+/* var n=14; 
+
+for (var numOfCount = 0; numOfCount < n; numOfCount++) {
+      document.write("Count " + numOfCount + "<br>");
+
+}
+
+document.write(`You have visited this site ${numOfCount} time`);  */ 
+
+/* ===========QUESTION NO 03 =============*/
+
+/* var birthYear = 1999;
+document.write(`My birth year is ${birthYear}<br><br>`); 
+document.write(`Data type of my declared variable is ${typeof birthYear}`) */
+
+/* ===========QUESTION NO 04 =============*/
+
+/* var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+
+var message = `${visitorName} ordered ${quantity} ${productTitle} on XYZ Clothing store.`
+document.write(message); */
